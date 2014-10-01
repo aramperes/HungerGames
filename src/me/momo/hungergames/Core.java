6b278217b96ecb107e243322e5c94d0bd7d0797b
Momo <aramax2000@hotmail.com>
2014-09-30 20:13:56 -0400

    Add Core class and package.

---
+++ src/me/momo/hungergames/Core.java
@@ -0,0 +1,19 @@
+package me.momo.hungergames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Copyright MOMOTHEREAL (c) 2014.
+ */
+public class Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Let's start some stuff here
+    }
+
+    @Override
+    public void onDisable() {
+        // Let's end some stuff here
+    }
+}
