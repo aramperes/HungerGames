e8e754263ff3a805e73e4d4cc35edc9bad966d5a
Momo <aramax2000@hotmail.com>
2014-10-02 12:27:50 -0400

    Add support for isPvP(), null checks.

--- src/me/momo/hungergames/game/event/PhaseListener.java
+++ src/me/momo/hungergames/game/event/PhaseListener.java
@@ -5,6 +5,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityEvent;
 import org.bukkit.event.player.*;
 
 /**
@@ -14,6 +16,8 @@ public class PhaseListener implements Listener {
 
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
+        if (Core.getPhaseManager().getCurrentPhase()==null)
+            return;
         if (!Core.getPhaseManager().getCurrentPhase().canJoin()) {
             event.disallow(PlayerLoginEvent.Result.KICK_OTHER, ChatColor.DARK_RED + "Sorry, but you cannot join during " + Core.getPhaseManager().getCurrentPhase().getDisplayName() + ".");
         }
@@ -21,20 +25,34 @@ public class PhaseListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        if (Core.getPhaseManager().getCurrentPhase()==null)
+            return;
         event.setJoinMessage(null);
         Core.getPhaseManager().getCurrentPhase().playerJoin(event.getPlayer());
     }
 
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent event) {
+        if (Core.getPhaseManager().getCurrentPhase()==null)
+            return;
         event.setQuitMessage(null);
         Core.getPhaseManager().getCurrentPhase().playerLeave(event.getPlayer());
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
+        if (Core.getPhaseManager().getCurrentPhase()==null)
+            return;
         if (!Core.getPhaseManager().getCurrentPhase().canMove())
             event.getPlayer().teleport(new Location
                     (event.getFrom().getWorld(), event.getFrom().getX(), event.getFrom().getY(), event.getFrom().getZ()));
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (Core.getPhaseManager().getCurrentPhase()==null)
+            return;
+        if (!Core.getPhaseManager().getCurrentPhase().isPvP())
+            event.setCancelled(true);
     }
 }
